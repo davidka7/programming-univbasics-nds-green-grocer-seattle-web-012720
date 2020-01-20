20326f44d4492b9d5b85c26d76c8b375b04ead92
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-20 07:45:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,6 +22,7 @@ def consolidate_cart(cart)
   new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
   if new_cart_item = !nil
     new_cart_item[:count] +=1 
+  else
     
 
 end 
