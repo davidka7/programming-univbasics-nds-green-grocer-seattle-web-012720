7019bbe07b594726ef3233da48a2e6b37064c3c5
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-20 08:22:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -77,7 +77,7 @@ end
 def checkout(items, coupons)
   cart = consolidate_cart(items)
   cart1 = apply_coupons(cart, coupons)
-  cart2 = apply_clearance(cart1)
+  cart2 = apply_clearance(couponed_cart)
   
   total = 0
   
