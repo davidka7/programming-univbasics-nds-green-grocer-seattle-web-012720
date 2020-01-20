691a2a54e3d4926da39039d87a3b15675d9294ee
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-20 08:14:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -51,7 +51,7 @@ def apply_coupons(cart, coupons)
    cart_item_with_coupon = {
   :item => couponed_item_name,
   :price => coupons[counter][:cost]/coupons[counter][:num],
-  :clearance => cart[counter][:clearance],
+  :clearance => cart[counter][:num],
   :count => 1
    }
   
