f58b7ee20be8d9af3295573063d45a0003317ed3
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-20 07:49:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,24 +16,22 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  new_cart=[]
-  counter=0 
-  while counter< cart.length
-  new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
-  if new_cart_item = !nil
-    new_cart_item[:count] +=1 
+   final_hash = {}
+  cart.each do |element_hash|
+    element_name = element_hash.keys[0]
+    element_stats = element_hash.values[0]
+
+    if final_hash.has_key?(element_name)
+      final_hash[element_name][:count] += 1
   else
-    new_cart_item = {
-      :item=> cart[counter][:item],
-      :price=> cart[counter][:price],
-      :clearance=> cart[counter][:clearance],
-      :count=> 1
-    }
-    new_cart << new_cart_item
-end
-counter+=1 
-end 
-new_cart 
+  final_hash[element_name] = {
+    count: 1,
+    price: element_stats[:price],
+    clearance: element_stats[:clearance]
+  } 
+  end
+ end
+  final_hash
 end
 
 def apply_coupons(cart, coupons)
