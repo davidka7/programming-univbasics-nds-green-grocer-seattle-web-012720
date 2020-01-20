c56a4022a201bc9f1ab6cb5bd2f866938da8e825
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-20 08:12:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,8 @@ def apply_coupons(cart, coupons)
     if cart_item_with_coupon
       cart_item_with_coupon[:count] += coupons[counter][:num]
  cart_item[:count] -= coupons[counter][:num]
- 
+ else
+   
   
 end
 
