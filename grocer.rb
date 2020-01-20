fd091e819b34581c8d2be90c2b6460459465d375
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-20 07:38:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,7 +2,7 @@ def find_item_by_name_in_collection(name, collection)
   counter=0 
   while counter< collection.length 
     if collection[counter][:item] == name
-      return collecction[counter]
+      return collection[counter]
     end
     counter+=1 
   end
